aff98bc2faf59605b06b8dab7b7f97e0e87fca8d
yakokino <yakokino@gmail.com>
2023-03-26 19:20:36 +0900

    テキスト変更&classに書き換えなど

--- js/app.js
+++ js/app.js
@@ -4,200 +4,143 @@ $SD.on('connected', conn => connected(conn));
 function connected(jsn) {
     debugLog('Connected Plugin:', jsn);
 
+    const action = new Action;
+
     /** subscribe to the willAppear event */
-    $SD.on('com.elgato.analogclock.action.willAppear', jsonObj =>
+    $SD.on('com.yakokino.natureremo.action1.willAppear', jsonObj =>
         action.onWillAppear(jsonObj)
     );
-    $SD.on('com.elgato.analogclock.action.didReceiveSettings', jsonObj =>
+    $SD.on('com.yakokino.natureremo.action1.didReceiveSettings', jsonObj =>
         action.onDidReceiveSettings(jsonObj)
     );
-    $SD.on('com.elgato.analogclock.action.willDisappear', jsonObj =>
+    $SD.on('com.yakokino.natureremo.action1.willDisappear', jsonObj =>
         action.onWillDisappear(jsonObj)
     );
-    $SD.on('com.elgato.analogclock.action.keyUp', jsonObj =>
+    $SD.on('com.yakokino.natureremo.action1.keyUp', jsonObj =>
         action.onKeyUp(jsonObj)
     );
 }
 
-var action = {
-    type: 'com.elgato.analogclock.action',
-    cache: {},
+class Action {
 
-    onDidReceiveSettings: function(jsn) {
-        console.log("onDidReceiveSettings", jsn);
+    constructor(name, age) {
+        this.type = 'com.yakokino.natureremo.action1';
+        this.cache = {};
+    }
+
+    onDidReceiveSettings(jsn) {
+        //console.log("onDidReceiveSettings", jsn);
         const settings = jsn.payload.settings;
         const clock = this.cache[jsn.context];
-        if(!settings || !clock) return;
+        if (!settings || !clock) return;
 
-        if(settings.hasOwnProperty('clock_index')) { /* if there's no clock-definitions, so simply do nothing */
-            /* set the appropriate clockface index as choosen from the popupmenu in PI */
-            const clockIdx = Number(settings.clock_index);
-            if(clock) {
-                clock.setClockFaceNum(clockIdx);
+        if (settings?.clock_index) {
+            if (clock) {
                 this.cache[jsn.context] = clock;
             }
         }
-        if(settings.hasOwnProperty('clock_type')) { /* if there's no clock-definitions, so simply do nothing */
-            if(clock) {
+        if (settings.hasOwnProperty('clock_type')) { /* if there's no clock-definitions, so simply do nothing */
+            if (clock) {
                 clock.setClockType(settings.clock_type);
             }
         }
+    }
 
-    },
+    onWillAppear(jsn) {
 
-    onWillAppear: function(jsn) {
+        if (!jsn?.payload?.settings) return;
 
-        if(!jsn.payload || !jsn.payload.hasOwnProperty('settings')) return;
+        //console.log('onWillAppear', jsn.payload.settings);
 
-        console.log('onWillAppear', jsn.payload.settings);
-
-        const clock = new AnalogClock(jsn);
         // cache the current clock
-        this.cache[jsn.context] = clock;
+        this.cache[jsn.context] = new AnalogClock(jsn);
         this.onDidReceiveSettings(jsn);
+    }
 
-    },
-
-    onWillDisappear: function(jsn) {
+    onWillDisappear(jsn) {
         let found = this.cache[jsn.context];
-        if(found) {
+        if (found) {
             // remove the clock from the cache
             found.destroyClock();
             delete this.cache[jsn.context];
         }
-    },
+    }
 
-    onKeyUp: function(jsn) {
+    onKeyUp(jsn) {
         const clock = this.cache[jsn.context];
         /** Edge case +++ */
-        if(!clock) this.onWillAppear(jsn);
+        if (!clock) this.onWillAppear(jsn);
         else clock.toggleClock();
     }
 
-};
+}
 
-function AnalogClock(jsonObj) {
-    var jsn = jsonObj,
-        context = jsonObj.context,
-        clockTimer = 0,
-        clock = null,
-        clockface = clockfaces[0],
-        currentClockFaceIdx = 0,
-        origContext = jsonObj.context,
-        canvas = null,
-        demo = false,
-        count = Math.floor(Math.random() * Math.floor(10)),
-        type = 'analog';
+class AnalogClock {
 
-
-    function isDemo() {
-        return demo;
+    constructor(jsonObj) {
+        this.jsn = jsonObj;
+        this.context = jsonObj.context;
+        this.clockTimer = 0;
+        this.clock = null;
+        this.origContext = jsonObj.context;
+        this.canvas = null;
+        this.demo = false;
+        this.count = Math.floor(Math.random() * Math.floor(10));
+        this.type = 'analog';
+        
+        this.createClock();
     }
 
-    function createClock(settings) {
-        canvas = document.createElement('canvas');
-        canvas.width = 144;
-        canvas.height = 144;
-        clock = new Clock(canvas);
-        clock.setColors(clockface.colors);
-        clock.type = type;
-        toggleClock();
+
+    isDemo() {
+        return this.demo;
     }
 
-    function toggleClock() {
+    createClock(settings) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = 144;
+        this.canvas.height = 144;
+        this.clock = new Clock(this.canvas);
+        this.clock.type = this.type;
+        this.toggleClock();
+    }
 
-        if(clockTimer === 0) {
-            clockTimer = setInterval(function(sx) {
+    toggleClock() {
 
-                if(demo) {
-                    let c = -1;
-                    if(count % 21 == 6) {
-                        c = 0;
-                    } else if(count % 21 === 3) {
-                        c = 1;
-                    } else if(count % 21 === 9) {
-                        c = 2;
-                    } else if(count % 21 === 12) {
-                        c = 3;
-                    } else if(count % 21 === 15) {
-                        c = 4;
-                    } else if(count % 21 === 18) {
-                        c = 5;
-                    }
-
-                    if(c !== -1) {
-                        setClockFaceNum(c, demo);
-                    } else {
-                        drawClock();
-                    }
-                } else {
-                    drawClock();
-                }
-
-                count++;
+        if (this.clockTimer === 0) {
+            this.clockTimer = setInterval((sx) => {
+                this.drawClock();
+                this.count++;
             }, 1000);
         } else {
-            window.clearInterval(clockTimer);
-            clockTimer = 0;
+            window.clearInterval(this.clockTimer);
+            this.clockTimer = 0;
         }
     }
 
-    function drawClock(jsn) {
-        clock.drawClock();
-        clockface.text === true && $SD.api.setTitle(context, new Date().toLocaleTimeString(), null);
+    drawClock(jsn) {
+        this.clock.drawClock();
+        $SD.api.setTitle(this.context, new Date().toLocaleTimeString(), null);
         $SD.api.setImage(
-            context,
-            clock.getImageData()
+            this.context,
+            this.clock.getImageData()
         );
     }
 
-    function setClockFace(newClockFace, isDemo) {
-        clockface = newClockFace;
-        demo = clockface.demo || isDemo;
-        clock.setColors(clockface.colors);
-        clockface.text !== true && $SD.api.setTitle(context, '', null);
-        drawClock();
-    }
-
-    function setClockFaceNum(idx, isDemo) {
-        currentClockFaceIdx = idx < clockfaces.length ? idx : 0;
-        this.currentClockFaceIdx = currentClockFaceIdx;
-        setClockFace(clockfaces[currentClockFaceIdx], isDemo);
-    }
-
-    function setClockType(type) {
-        clock.type = type;
+    setClockType(type) {
+        this.clock.type = type;
         this.drawClock();
     }
 
-    function getClockType() {
-        clock.type;
+    getClockType() {
+        this.clock.type;
     }
 
-    function destroyClock() {
-        if(clockTimer !== 0) {
+    destroyClock() {
+        if (clockTimer !== 0) {
             window.clearInterval(clockTimer);
-            clockTimer = 0;
+            this.clockTimer = 0;
         }
     }
 
-    createClock();
-
-    return {
-        clock: clock,
-        clockTimer: clockTimer,
-        clockface: clockface,
-        currentClockFaceIdx: currentClockFaceIdx,
-        name: name,
-        drawClock: drawClock,
-        toggleClock: toggleClock,
-        origContext: origContext,
-        setClockFace: setClockFace,
-        setClockFaceNum: setClockFaceNum,
-        destroyClock: destroyClock,
-        demo: demo,
-        isDemo: isDemo,
-        setClockType: setClockType,
-        getClockType: getClockType
-    };
 }
